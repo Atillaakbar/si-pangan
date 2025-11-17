f32d83fb4fe7e8a9ddf548769d093e3d05b35113
rislarasati5 <lrstii05@gmail.com>
2025-11-17 17:45:50 +0700

    Skema 2 - Tambahan baris dari Laras

--- Home.php
+++ Home.php
@@ -6,6 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        echo "Baris tambahan dari Zahira - Skema 2";  // ← Tambahan skema 2
+        return "Edit dari Zahira - Skema 1";          // ← Skema 1 tetap ada
     }
 }
