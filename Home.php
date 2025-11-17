3cd5a38a20681148c62d21f7038c1fb0b5f012b9
Atillaakbar <akbartawaqal27@gmail.com>
2025-11-17 17:56:51 +0700

    Skema 2 - Tambahan baris dari Atila

--- Home.php
+++ Home.php
@@ -6,6 +6,17 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+       
+        // SKEMA 2 → Tambah baris berbeda
+      
+
+
+        
+        
+echo "Tambahan dari Atila - Skema 2<br>";    // Baris untuk Anggota 2
+   
+        // SKEMA 1 → Semua edit baris yang sama
+     
+        return "EDIT SKEMA 1 (semua anggota mengubah tulisan ini)";
     }
 }
