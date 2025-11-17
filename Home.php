428979ffb4d4aa65600871df7b80d0e729409fd9
Yoga Pratama <pulaga748@gmail.com>
2025-11-17 18:13:40 +0700

    Selesaikan merge dari remote

--- Home.php
+++ Home.php
@@ -9,6 +9,7 @@ class Home extends BaseController
        
         // SKEMA 2 → Tambah baris berbeda
 <<<<<<< HEAD
+<<<<<<< HEAD
         
 
         echo "skema 2 - Tambahan baris dari Zahira";      // Baris untuk Zahira
@@ -31,5 +32,17 @@ echo "Tambahan dari Atila - Skema 2<br>";    // Baris untuk Anggota 2
      
         return "EDIT SKEMA 1 (semua anggota mengubah tulisan ini)";
 >>>>>>> 6f64b7ac84880fbcc8fe6e3e0ca4039b9a8eca2b
+=======
+  
+
+
+        echo "Tambahan dari Yogaa - Skema 2<br>";    // Baris untuk Yogaa
+
+
+        // SKEMA 1 → Semua edit baris yang sama
+        
+
+        return "EDIT SKEMA 1 (semua anggota mengubah tulisan ini)";
+>>>>>>> 8f93243 (Skema 1: Yogaa mengubah baris yang sama di Home.php)
     }
 }
