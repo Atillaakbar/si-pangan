8f932439e6d89649b6fb6a61fb006499921b7cc4
Yoga Pratama <pulaga748@gmail.com>
2025-11-17 17:51:06 +0700

    Skema 1: Yogaa mengubah baris yang sama di Home.php

--- Home.php
+++ Home.php
@@ -6,6 +6,17 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+       
+        // SKEMA 2 → Tambah baris berbeda
+  
+
+
+        echo "Tambahan dari Yogaa - Skema 2<br>";    // Baris untuk Yogaa
+
+
+        // SKEMA 1 → Semua edit baris yang sama
+        
+
+        return "EDIT SKEMA 1 (semua anggota mengubah tulisan ini)";
     }
 }
