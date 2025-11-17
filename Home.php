a63df8dda79440212ea2159f2300cfa2a261fa5e
ghaitzazahiraa <ghaitzazahiraa27@gmail.com>
2025-11-17 17:38:44 +0700

    Skema 2 - Tambahan baris dari Zahira

--- Home.php
+++ Home.php
@@ -6,6 +6,17 @@ class Home extends BaseController
 {
     public function index(): string
     {
+       
+        // SKEMA 2 → Tambah baris berbeda
+        
+
+        echo "Tambahan dari Zahira - Skema 2<br>";      // Baris untuk Zahira
+        
+
+        
+        
+      
+
         return "Edit dari Zahira - Skema 1";
     }
 }
